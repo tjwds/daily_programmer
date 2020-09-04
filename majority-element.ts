0c382d36b286a3413d408c522fdb00b9f3a57260
Joe Woods <code@joewoods.dev>
2020-09-03 20:16:47 -0400

    Fix broken test on even-length elements

--- majority-element.ts
+++ majority-element.ts
@@ -1,10 +1,18 @@
 const majorityElement = (arrUnsorted: number[]):number => {
-    if (arrUnsorted.length) {
+    const arrayLength = arrUnsorted.length;
+    if (arrayLength) {
         const arr = arrUnsorted.sort();
-        const middle = Math.floor(arr.length / 2);
-        const middleElement = arr[middle];
-        if (middleElement === arr[0] || middleElement === arr[arr.length - 1]) {
-            return middleElement;
+        const middle = Math.ceil(arrayLength / 2);
+        const rightMiddleElement = arr[middle - 1];
+        const leftMiddleElement = arrayLength % 2 ?
+            rightMiddleElement :
+            arr[middle];
+        console.log(arr, leftMiddleElement, rightMiddleElement)
+        if (leftMiddleElement === arr[0]) {
+            return leftMiddleElement;
+        }
+        if (rightMiddleElement === arr[arrayLength - 1]) {
+            return rightMiddleElement;
         }
     }
     return -1;
@@ -13,3 +21,6 @@ const majorityElement = (arrUnsorted: number[]):number => {
 console.assert(majorityElement([4,5,6,3,5,3,3,3,3,3]) === 3);
 console.assert(majorityElement([1]) === 1);
 console.assert(majorityElement([1,2,3]) === -1);
+console.assert(majorityElement([0,0,2,2]) === -1);
+console.assert(majorityElement([0,2,2,2]) === 2);
+console.assert(majorityElement([0,0,0,2]) === 0);
